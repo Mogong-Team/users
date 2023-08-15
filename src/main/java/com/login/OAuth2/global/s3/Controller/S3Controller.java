a78643562237818de33e8200f84a39f17495e472
ABCganada <shinmg9712@gmail.com>
2023-08-15 16:50:57 +0900

    fix: imageUrl read download 현상 수정

--- src/main/java/com/login/OAuth2/global/s3/Controller/S3Controller.java
+++ src/main/java/com/login/OAuth2/global/s3/Controller/S3Controller.java
@@ -2,6 +2,7 @@ package com.login.OAuth2.global.s3.Controller;
 
 import com.login.OAuth2.global.s3.service.S3Service;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,18 +12,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+@Slf4j
 @RestController
-@RequestMapping("/upload")
+@RequestMapping("/file")
 @RequiredArgsConstructor
-public class FileUploadController {
+public class S3Controller {
 
     private final S3Service s3Service;
 
-    @PostMapping
+    @PostMapping("/upload")
     public ResponseEntity<String> uploadFile(@RequestParam("imageFile") MultipartFile file){
 
         try{
             String imageUrl = s3Service.upload(file);
+            log.info("imageUrl : " + imageUrl);
 
             HttpHeaders responseHeaders = new HttpHeaders();
             responseHeaders.add("image-url", imageUrl);
