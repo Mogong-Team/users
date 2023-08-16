d7f2fa1a7750263aaf91159cbc1697da68fa22f5
ABCganada <shinmg9712@gmail.com>
2023-08-16 23:44:05 +0900

    fix : 이미지 업로드, 삭제 오류 수정

--- src/main/java/com/login/OAuth2/global/s3/service/S3Service.java
+++ src/main/java/com/login/OAuth2/global/s3/service/S3Service.java
@@ -1,49 +1,67 @@
 package com.login.OAuth2.global.s3.service;
 
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.login.OAuth2.domain.user.util.ProfileImageUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 @Slf4j
 @RequiredArgsConstructor
-@Transactional
 @Service
 public class S3Service {
 
     private static String bucket = "mogong-s3-bucket";
-
     private final AmazonS3Client amazonS3Client;
 
-    /**
-     * S3 파일 업로드
-     * */
     public String upload(MultipartFile multipartFile) throws IOException {
 
-        String fileName = UUID.randomUUID() + "-" + multipartFile.getOriginalFilename();
+        String fileName = generateUniqueFileName(multipartFile.getOriginalFilename());
 
         ObjectMetadata objectMetadata = new ObjectMetadata();
         objectMetadata.setContentType(multipartFile.getContentType());
         objectMetadata.setContentLength(multipartFile.getSize());
 
-        amazonS3Client.putObject(bucket, fileName, multipartFile.getInputStream(), objectMetadata);
+        try(InputStream inputStream = multipartFile.getInputStream()){
+            amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, inputStream, objectMetadata)
+                    .withCannedAcl(CannedAccessControlList.PublicRead));
+        }
 
         return amazonS3Client.getUrl(bucket, fileName).toString();
     }
 
-    public void delete(String imageUrl){
+    private String generateUniqueFileName(String originalFileName){
+        String extension = originalFileName.substring(originalFileName.lastIndexOf("."));
+        return UUID.randomUUID().toString() + extension;
+    }
 
-        if(amazonS3Client.doesObjectExist(bucket, imageUrl))
-        amazonS3Client.deleteObject(bucket, imageUrl);
+    public void delete(String imageUrl) {
+
+        String objectKey = getObjectKeyFromUrl(imageUrl);
+        objectKey = URLDecoder.decode(objectKey, StandardCharsets.UTF_8);
+
+        if(!ProfileImageUtil.PROFILE_IMAGE_URLS.contains(imageUrl)) {
+            try {
+                amazonS3Client.deleteObject(bucket, objectKey);
+            } catch (AmazonS3Exception e) {
+                log.info("Failed file delete : {}", e.getMessage());
+            }
+        }
+    }
+
+    private String getObjectKeyFromUrl(String imageUrl){
+        return imageUrl.substring(imageUrl.lastIndexOf("/") + 1);
     }
 }
 
